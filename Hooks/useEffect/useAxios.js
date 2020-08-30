ca2ecb8e23b714faf254406ba8855c15a4e325ae
Candice Han <jeeyoung.han82@gmail.com>
2020-08-30 21:30:44 +0200

    ADD useAxios.js

---
+++ Hooks/useEffect/useAxios.js
@@ -0,0 +1,38 @@
+import defaultAxios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const useAxios = (opts, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null,
+  });
+  const [trigger, setTrigger] = useState(0);
+  useEffect(() => {
+    axiosInstance(opts)
+      .then((data) => {
+        setState({
+          ...state,
+          loading: false,
+          data,
+        });
+      })
+      .catch((error) => {
+        setState({ ...state, loading: false, error });
+      });
+  }, [trigger]);
+  if (!opts.url) {
+    return;
+  }
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    setTrigger(new Date());
+  };
+  return { ...state, refetch };
+};
+
+export default useAxios;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
-
 const App = () => {
   return <div className="App"></div>;
 };
