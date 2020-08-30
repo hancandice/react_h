c66be4c8d6b55453b930936bca6a4c8b1a296b33
Candice Han <jeeyoung.han82@gmail.com>
2020-08-30 14:10:21 +0200

    ADD useScroll.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
 
 const App = () => {
